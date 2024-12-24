d719935cdd614f93e4e9658d0cb975071ca4b510
bailangvvkg <bailangvvking@outlook.com>
2024-12-24 23:26:34 +0800

    Update Packages.sh
    不更新daed

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -41,7 +41,7 @@ UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/packages" "main" "pkg"
 
 # 大鹅 daed
-UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "master" "pkg"
+# UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "master" "pkg"
 
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
