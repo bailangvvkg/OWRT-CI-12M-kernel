83452f6cc8c7e94223ef9bcf6f7bb81013c8d213
bailangvvkg <bailangvvking@outlook.com>
2025-03-26 12:57:37 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -216,3 +216,5 @@ echo "CONFIG_PACKAGE_git-http=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-haproxy-tcp=y" >> ./.config
 # Adguardhome去广告
 echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> ./.config
+# cloudflre速度筛选器
+echo "CONFIG_PACKAGE_luci-app-cloudflarespeedtest=y" >> ./.config
