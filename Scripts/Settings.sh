c63c00d117715acebbf53db3aedf875cf70fc273
bailangvvkg <bailangvvking@outlook.com>
2025-05-27 11:18:45 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -233,7 +233,7 @@ echo "CONFIG_PACKAGE_git-http=y" >> ./.config
 # Nginx替换Uhttpd
 echo "CONFIG_PACKAGE_nginx-mod-luci=y" >> ./.config
 # Nginx的图形化界面
-# echo "CONFIG_PACKAGE_luci-nginxer=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-nginx=y" >> ./.config
 # HAProxy 比Nginx更强大的反向代理服务器
 echo "CONFIG_PACKAGE_luci-app-haproxy-tcp=y" >> ./.config
 # Adguardhome去广告
